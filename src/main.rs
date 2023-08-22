892ecb4001b09876e2ecc02072470c3c59ce3644
Patrick Li <baichuanli@yahoo.com>
2023-08-22 02:41:14 -0400

    feat: nats first trial

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 #[macro_use] extern crate rocket;
+use rocket::State;
+use nats::Connection;
 
+struct TNStates {
+    nats: Connection
+}
 #[get("/")]
-fn index() -> &'static str {
+fn index(connections: &State<TNStates>) -> &'static str {
+    connections.nats.publish("foo", "Hello World!").expect("Failed to publish");
     "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    let nc : nats::Connection= nats::connect("demo.nats.io").expect("Failed to connect to NATS");
+    let connections = TNStates { nats: nc };
+    rocket::build().mount("/", routes![index]).manage(connections)
 }
