a2de041651df725ac402c7523e42fe7d39351c84
Patrick Li <baichuanli@yahoo.com>
2023-09-06 09:28:25 -0400

    fmt: code format

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,7 @@ async fn manual_hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(hello).service(echo).route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
